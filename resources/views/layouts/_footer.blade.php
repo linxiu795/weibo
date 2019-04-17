2563be9d9e08b7aaf703d7866e5eab6005900f3d
linxiu795 <772352009@qq.com>
2019-04-17 17:44:05 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/images/201806/12/1/Owic58YjaW.jpeg?imageView2/2/w/1240/h/0" alt="">
+  <a href="https://laravel-china.org/courses" target="_blank">
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
