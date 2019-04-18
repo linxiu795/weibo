88325b6eb3d841a7664898b64ba206a4582383a9
linxiu795 <772352009@qq.com>
2019-04-18 04:04:43 +0000

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,7 +8,8 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
-      <form method="POST" action="{{ route('users.store') }}"></form>
+      <form method="POST" action="{{ route('users.store') }}">
+        {{ csrf_field() }}
       <div class="form-group">
         <label for="name">名称：</label>
         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
@@ -26,10 +27,11 @@
 
       <div class="form-group">
         <label for="password_confirmation">确认密码：</label>
-        <input type="password" name="password_confirmation" class="form-control">
+        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
       </div>
 
       <button type="submit" class="btn btn-primary">注册</button>
+      </form>
     </div>
   </div>
 </div>
