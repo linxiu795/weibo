964f0317366c2c25a0b4bdca4132adb3667dbb63
linxiu795 <772352009@qq.com>
2019-04-27 01:10:35 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'kunbin145';
         $user->email = 'kunbin145@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
